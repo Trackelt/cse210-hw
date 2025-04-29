87b5df0588bc75591e0a54c362807b493d22e4ca
Trackelt <tra20026@byui.edu>
2025-04-29 10:34:35 -0700

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //string magic1 = Console.ReadLine();
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+        //int magic = int.Parse(magic1);
+
+        int guess;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess1 = Console.ReadLine();
+            guess = int.Parse(guess1);
+
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magic);
     }
 }
