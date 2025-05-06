29252c7c1c2459164be6770e5beec74544fc238d
Trackelt <tra20026@byui.edu>
2025-05-06 15:36:58 -0700

    Finished the Resume prep

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+        //job1.DisplayJobDetails();
+        Job job2 = new Job();
+        job2._jobTitle = "Mathematician";
+        job2._company = "BYUI";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+        //job2.DisplayJobDetails();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Bob";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        //Console.WriteLine($"{resume1._jobs[0]._jobTitle}");
+        resume1.DisplayResume();
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,38 @@
 using System;
 
+class Circle
+{
+    private double _radius;
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        //Console.WriteLine("Hello Sandbox World!");
+
+        Circle myCircle = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine($"{myCircle.GetRadius()}");
+        
+        Console.WriteLine($"{myCircle.GetArea()}");
+
+
+
     }
 }
